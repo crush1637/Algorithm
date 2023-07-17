10420db47c7b76297c4e0bb74db87b4846c793cc
LEE <divdev@kaoni.com>
2023-07-17 09:53:47 +0900

    commit

---
+++ src/main/java/WEEK2/DAY6.java
@@ -0,0 +1,21 @@
+package WEEK2;
+
+import java.util.Scanner;
+
+public class DAY6 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        int C = A + B;
+        int D = A - B;
+        int E = A * B;
+        int F = A / B;
+        int G = A % B;
+        System.out.println(C);
+        System.out.println(D);
+        System.out.println(E);
+        System.out.println(F);
+        System.out.println(G);
+    }
+}
