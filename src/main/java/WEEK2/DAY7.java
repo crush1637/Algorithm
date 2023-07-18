b5a4c2bb91bbc24cdf9da3c933a4aa31ea9cf36d
LEE <divdev@kaoni.com>
2023-07-18 10:01:29 +0900

    commit

---
+++ src/main/java/WEEK2/DAY7.java
@@ -0,0 +1,11 @@
+package WEEK2;
+
+import java.util.Scanner;
+
+public class DAY7 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String id = scanner.nextLine();
+        System.out.println(id + "??!");
+    }
+}
